8d4d005b9b1d8f3c31eed694ba526945c1399bd9
Jayson Reales <115851159+jaysonrealesdev@users.noreply.github.com>
2022-11-08 16:04:46 +0800

    update

--- script.js
+++ script.js
@@ -80,3 +80,25 @@ form.addEventListener("submit", e => {
   form.reset();
   input.focus();
 });
+
+var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "102225458423607");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v15.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
